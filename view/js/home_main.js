6c35e7acad5c339b01523cdf5a23524f31117c8f
josefhsilva815-byte <josefhsilva815@gmail.com>
2025-11-21 09:28:55 -0300

    Criação da pasta View para as páginas do site, nela temos uma pasta para html, cs e js, criado a página de Login, além de um arquivo de apoio para escolher fontes do google.

---
+++ view/js/home_main.js
@@ -0,0 +1,9 @@
+
+const input = {
+    email: document.getElementById("email"),
+    senha: document.getElementById("senha") 
+};
+
+document.getElementById("enviar").addEventListener("click", () => {
+    
+})
